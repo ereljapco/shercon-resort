b61e5e0844798d9fc864817bd3222692a7d2a92e
Erel E. Ropeta-Japco <44773736+ereljapco@users.noreply.github.com>
2024-06-29 21:57:31 +0800

    indicate the `namespace` before the `enum`s

--- Models/Room.cs
+++ Models/Room.cs
@@ -1,66 +1,74 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace SherconResort.Web.Models
+namespace SherconResort.Web.Models;
+public enum AccomodationType
 {
-	public enum AccomodationType
-	{
-		Daytour, Overnight
-	}
-	public enum RoomType
-	{
-		Kubo, Benches, Pavilion, Hall
-	}
-	public class Room
-	{
-		[Key]
-		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-		public int Id { get; set; }
+	[Display(Name = "Daytour")]
+	Daytour,
+	[Display(Name = "Overnisght")]
+	Overnight
+}
+public enum RoomType
+{
+	[Display(Name = "Kubo")]
+	Kubo,
+	[Display(Name = "Benches")]
+	Benches,
+	[Display(Name = "Pavilion")]
+	Pavilion,
+	[Display(Name = "Hall")]
+	Hall
+}
+public class Room
+{
+	[Key]
+	[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+	public int Id { get; set; }
 
-		[Required]
-		[StringLength(100, ErrorMessage = "Room name cannot exceed 100 characters.")]
-		public required string Name { get; set; }
+	[Required]
+	[StringLength(100, ErrorMessage = "Room name cannot exceed 100 characters.")]
+	public required string Name { get; set; }
 
-		[StringLength(250, ErrorMessage = "Description cannot exceed 250 characters.")]
-		public string? Description { get; set; }
+	[StringLength(250, ErrorMessage = "Description cannot exceed 250 characters.")]
+	public string? Description { get; set; }
 
-		[Required]
-		[EnumDataType(typeof(AccomodationType))]
-		public required string AccomodationType { get; set; }
+	[Required]
+	[EnumDataType(typeof(AccomodationType))]
+	public required string AccomodationType { get; set; }
 
-		[Required]
-		[EnumDataType(typeof(RoomType))]
-		public required string RoomType { get; set; }
+	[Required]
+	[EnumDataType(typeof(RoomType))]
+	public required string RoomType { get; set; }
 
-		[Required]
-		[Range(1, 100, ErrorMessage = "The maximum number of occupants must be between 1 and 100.")]
-		public int MaxOccupants { get; set; }
+	[Required]
+	[Range(1, 100, ErrorMessage = "The maximum number of occupants must be between 1 and 100.")]
+	public int MaxOccupants { get; set; }
 
-		[Range(0, 10, ErrorMessage = "The number of bedrooms must be between 1 and 10.")]
-		public int NumberofBedrooms { get; set; }
+	[Range(0, 10, ErrorMessage = "The number of bedrooms must be between 1 and 10.")]
+	public int NumberofBedrooms { get; set; }
 
-		[Range(0, 10, ErrorMessage = "The number of beds must be between 1 and 10.")]
-		public int NumberOfBeds { get; set; }
+	[Range(0, 10, ErrorMessage = "The number of beds must be between 1 and 10.")]
+	public int NumberOfBeds { get; set; }
 
-		[Range(0, 10, ErrorMessage = "The number of bathrooms must be between 1 and 10.")]
-		public int NumberOfBathRooms { get; set; }
+	[Range(0, 10, ErrorMessage = "The number of bathrooms must be between 1 and 10.")]
+	public int NumberOfBathRooms { get; set; }
 
-		public bool HasKitchen { get; set; }
+	public bool HasKitchen { get; set; }
 
-		public bool HasAircon { get; set; }
+	public bool HasAircon { get; set; }
 
-		[Required]
-		[Range(1.00, 10000.00, ErrorMessage = "The price must be between 1.00 and 10000.00.")]
-		[Column(TypeName = "decimal(18, 2)")]
-		public decimal Price { get; set; }
+	[Required]
+	[Range(1.00, 10000.00, ErrorMessage = "The price must be between 1.00 and 10000.00.")]
+	[Column(TypeName = "decimal(18, 2)")]
+	public decimal Price { get; set; }
 
-		[DataType(DataType.Date)]
-		public DateTime CreateAt { get; set; }
+	[DataType(DataType.Date)]
+	public DateTime CreateAt { get; set; }
 
-		[DataType(DataType.Date)]
-		public DateTime UpdatedAt { get; set; }
+	[DataType(DataType.Date)]
+	public DateTime UpdatedAt { get; set; }
 
-		public ICollection<Reservation>? Reservations { get; set; }
-		public ICollection<Review>? Reviews { get; set; }
-	}
+	public ICollection<Reservation>? Reservations { get; set; }
+	public ICollection<Review>? Reviews { get; set; }
 }
